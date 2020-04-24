79826d90efe21172db978fff7fd181d791f623fb
Magnus Larsson <maglars2@gmail.com>
2020-04-24 17:13:52 +0200

    Some healthy refactoring
    Use card-tools

--- dist/vasttrafik-card.js
+++ dist/vasttrafik-card.js
@@ -1,139 +1,146 @@
-const LitElement = Object.getPrototypeOf(customElements.get("hui-view"));
-const html = LitElement.prototype.html;
+customElements.whenDefined('card-tools').then(() => {
+    const ct = customElements.get('card-tools');
 
-class VasttrafikCard extends LitElement {
-  static get properties() {
-    return {
-      _config: {
-        entities: []
-      },
-      _hass: {
-        states: []
-      }
-    };
-  }
+    class VasttrafikCard extends ct.LitElement {
 
-  setConfig(config) {
-    if (!config.entities || config.entities.length === 0) {
-      throw new Error("Specify at least one entity!");
-    }
-
-    this._entities = config.entities.map(entity => {
-      if (typeof entity === 'string') {
-        return {'id': entity, 'delay': 0};
-      } else {
-        return Object.assign({}, entity);
-      }
-    });
-
-    this._config = config;
-  }
-
-  set hass(hass) {
-    this._hass = hass;
-
-    if (!this._isVerified) {
-      this.verifyEntities();
-      this._isVerified = true;
-    }
-
-    this.createCardTemplates();
-  }
-
-  verifyEntities() {
-    this._entities
-      .filter(entity => !!this._hass.states[entity.id])
-      .forEach(entity => {
-        const attribution = this._hass.states[entity.id].attributes.attribution;
-
-        if (!attribution || !attribution.toLowerCase().includes('västtrafik')) {
-          console.warn(`WARNING: ${entity.id} does not seem to be a Västtrafik-sensor. Instead it is attributed to ${attribution}`);
+        static get properties() {
+            return {
+                config: {},
+                hass: {},
+            };
         }
-      });
-  }
 
-  createCardTemplates() {
-    this._entities = this._entities
-      .filter(entity => !!this._hass.states[entity.id])
-      .map(entity => Object.assign({}, {'departureTime': this._hass.states[entity.id].state}, entity));
+        setConfig(config) {
+            this.title = this.config.title || 'Västtrafik';
+            this.entities = this._parseEntities(config.entities);
+            this.config = config;
+        }
 
-    this._entities.sort((a,b) => this.getTimeUntil(a) - this.getTimeUntil(b));
-  }
+        _parseEntities(configuredEntities) {
+            return configuredEntities.map(entity => {
+                if (typeof entity === 'string') {
+                    return {'id': entity, 'delay': 0};
+                } else {
+                    return Object.assign({}, entity);
+                }
+            });
+        }
 
-  render() {
-    const title = this._config.title || 'Västtrafik';
-    const renderedEntities = this._entities.map(entity => this.renderEntity(entity));
+        set hass(hass) {
+            this.hass = hass;
 
-    return html`
-      <link type="text/css" rel="stylesheet" href="/community_plugin/lovelace-vasttrafik-card/vasttrafik-card.css"></link>
-      <ha-card>
-        <div class="card-header">
-          ${title}
-        </div>
-        <div>
-          <table>
-            <tr>
-              <th align="left"></th>
-              <th align="left">Time</th>
-              <th align="left">From</th>
-              <th align="left">Leave home</th>
-            </tr>
-            ${renderedEntities}
-          </table>
-        </div>
-      </ha-card>`;
-  }
+            if (!this.isVerified) {
+                this._verifyEntities();
+                this.isVerified = true;
+            }
+        }
 
-  renderEntity(entity) {
-    if (!(entity.id in this._hass.states)) {
-       return;
+        _verifyEntities() {
+            this.entities
+                .filter(entity => !!this.hass.states[entity.id])
+                .forEach(entity => {
+                    const attribution = this.hass.states[entity.id].attributes.attribution;
+
+                    if (!attribution || !attribution.toLowerCase().includes('västtrafik')) {
+                        console.warn(`WARNING: ${entity.id} does not seem to be a Västtrafik-sensor. Instead it is attributed to ${attribution}`);
+                    }
+                });
+        }
+
+        render() {
+            this._sortEntities();
+            const renderedEntities = this.entities.map(entity => this._renderEntity(entity));
+
+            return ct.LitHtml`
+	            <link type="text/css" rel="stylesheet" href="/community_plugin/lovelace-vasttrafik-card/vasttrafik-card.css"></link>
+	            <ha-card>
+	                <div class="card-header">
+	                    ${this.title}
+	                </div>
+	                <div>
+	                    <table>
+	                        <tr>
+	                            <th align="left"></th>
+	                            <th align="left">Time</th>
+	                            <th align="left">From</th>
+	                            <th align="left">Leave home</th>
+	                        </tr>
+	                        ${renderedEntities}
+	                    </table>
+	                </div>
+	            </ha-card>`;
+        }
+
+        _sortEntities() {
+            this.entities = this.entities
+                .filter(entity => entity.id in this.hass.states)
+                .map(entity => Object.assign({}, {'departureTime': this.hass.states[entity.id].state}, entity));
+
+            this.entities.sort((a, b) => this.getTimeUntil(a) - this.getTimeUntil(b));
+        }
+
+        _renderEntity(entity) {
+            if (!(entity.id in this.hass.states)) {
+                return;
+            }
+
+            const hassEntity = this.hass.states[entity.id];
+            const attributes = hassEntity.attributes;
+
+            const line = attributes.line;
+            const lineClass = this._getLineClass(line);
+            const departureTime = hassEntity.state;
+            const timeUntilLeave = this._getTimeUntil(entity);
+            const from = attributes.from || '';
+
+            return ct.LitHtml`<tr>
+                            <td class="${lineClass} line">${line}</td>
+                            <td>${departureTime}</td>
+                            <td>${from}</td>
+                            <td>${timeUntilLeave} minutes</td>
+                        </tr>`;
+        }
+
+        _getLineClass(line) {
+            const tramLines = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '13'];
+            const busRapidTransits = ['16', '17', '18', '19', '25', '50', '52', '60'];
+
+            if (tramLines.indexOf(line) !== -1) {
+                return `line-${line}`;
+            } else if (busRapidTransits.indexOf(line) !== -1) {
+                return 'bus-rapid-transit';
+            } else {
+                return 'regular-line';
+            }
+        }
+
+        _getTimeUntil(entity) {
+            const now = new Date();
+            const nowHour = now.getHours();
+            const nowMinute = now.getMinutes();
+            const expectedTime = entity.departureTime.split(':');
+            const expectedHour = parseInt(expectedTime[0]);
+            const expectedMinute = parseInt(expectedTime[1]);
+            let hourDiff = expectedHour < nowHour ? 24 + (expectedHour - nowHour) : expectedHour - nowHour;
+            let minuteDiff = expectedMinute - nowMinute;
+            if (minuteDiff < 0) {
+                minuteDiff += 60;
+                hourDiff--;
+            }
+
+            return hourDiff * 60 + minuteDiff - entity.delay;
+        }
     }
 
-    const hassEntity = this._hass.states[entity.id];
-    const attributes = hassEntity.attributes;
+    customElements.define('vasttrafik-card', VasttrafikCard);
+});
 
-    const line = attributes.line;
-    const lineClass = this.getLineClass(line);
-    const departureTime = hassEntity.state;
-    const timeUntilLeave = this.getTimeUntil(entity);
-    const from = attributes.from || '';
-
-    return html`<tr>
-              <td class="${lineClass} line">${line}</td>
-              <td>${departureTime}</td>
-              <td>${from}</td>
-              <td>${timeUntilLeave} minutes</td>
-            </tr>`;
-  }
-
-  getLineClass(line) {
-    const tramLines = ['1','2','3','4','5','6','7','8','9','10','11','13'];
-    const busRapidTransits = ['16','17','18','19','25','50','52','60'];
-
-    if(tramLines.indexOf(line) !== -1) {
-      return `line-${line}`;
-    } else if(busRapidTransits.indexOf(line) !== -1) {
-      return 'bus-rapid-transit';
-    } else {
-      return 'regular-line';
+setTimeout(() => {
+    if (!customElements.get('card-tools')) {
+        customElements.define('vasttrafik-card', class extends HTMLElement {
+            setConfig() {
+                throw new Error("Can't find card-tools. See https://github.com/thomasloven/lovelace-card-tools");
+            }
+        });
     }
-  }
-
-  getTimeUntil(entity) {
-    const now = new Date();
-    const nowHour = now.getHours();
-    const nowMinute = now.getMinutes();
-    const expectedTime = entity.departureTime.split(':');
-    const expectedHour = parseInt(expectedTime[0]);
-    const expectedMinute = parseInt(expectedTime[1]);
-    let hourDiff = expectedHour < nowHour ? 24+(expectedHour-nowHour) : expectedHour-nowHour;
-    let minuteDiff = expectedMinute-nowMinute;
-    if(minuteDiff < 0) {
-      minuteDiff += 60;
-      hourDiff--;
-    }
-
-    return hourDiff*60 + minuteDiff - entity.delay;
-  }
-}
-customElements.define('vasttrafik-card', VasttrafikCard);
+}, 2000);
